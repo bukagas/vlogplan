cff4101938cef442dab19b98196ff8c75b89a33f
Seno Priambodo <bukagas1988@gmail.com>
2022-08-09 22:04:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati natus dolor neque cumque fugiat et at sequi, nesciunt harum aliquam voluptas aliquid, autem accusamus fugit, similique praesentium reiciendis laborum perferendis!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
